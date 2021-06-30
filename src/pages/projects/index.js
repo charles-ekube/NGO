a71721886086bfbd67073f709cc2aeb0a6a8ecaf
charles-ekube <charleswakili797@gmail.com>
2021-06-30 16:20:21 +0100

    updated responsiveness for tablet and mobile version

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,49 +1,49 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { JoinUsHero, Val } from '../../assets';
-import { Navbar, Footer, JoinUs, ShowMoreBtn, Card } from '../../components';
-import './style.css';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { JoinUsHero, Val } from "../../assets";
+import { Navbar, Footer, JoinUs, ShowMoreBtn, Card } from "../../components";
+import "./style.css";
 
 const Projects = () => {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <section className="projects-intro-container">
+          <div className="projects-intro-overlay">
+            <div className="projects-overlay-text">
+              <h2>We have accomplished a lot and enriched lives.</h2>
+              <p>
+                Lets come together and eradicate poverty and hunger once and for
+                all from our communities
+              </p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className="projects-page-grid">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </div>
 
-    return (
-        <>
-            <Navbar/>
-            <main>
-            <section className='projects-intro-container'>
-                    <div className='projects-intro-overlay'>
-                        <div className='projects-overlay-text'>
-                            <h2>
-                                We have accomplished a lot and enriched lives.
-                            </h2>
-                            <p>
-                                Lets come together and eradicate poverty and hunger once and for all from our communities
-                            </p>
-                        </div>
-                    </div>
-                </section>
-                <section className='projects-page-grid'>
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <div className='show-more-container'>
-                        <ShowMoreBtn/>
-                    </div>
-                </section>
-                <section>
-                    <JoinUs bgImg={JoinUsHero}/>
-                </section>
-            </main>
-            <Footer/>
-        </>
-    )
-}
+          <div className="show-more-container">
+            <ShowMoreBtn />
+          </div>
+        </section>
+        <section>
+          <JoinUs bgImg={JoinUsHero} />
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-export {Projects}
+export { Projects };
